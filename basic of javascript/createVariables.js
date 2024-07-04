4cf93f9092b148db38a74350e170ae718f808cda
nirmal <nirmaljaiswal888@gmail.com>
2024-07-04 21:18:24 +0530

    create the variables in javascript.

---
+++ basic of javascript/createVariables.js
@@ -0,0 +1,12 @@
+//create the variables in javascript.//
+a=10;
+console.log(a);
+a=20.7;
+console.log(a);
+a ="nirmal";
+console.log(a);
+//there is no need to mention datatypes in js//
+b=6;
+console.log(b);
+b++;
+console.log(b);
