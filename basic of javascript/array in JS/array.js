3fe5c0039804730f279bd81a905eb2d9a9763cea
nirmal <nirmaljaiswal888@gmail.com>
2024-07-06 03:18:34 +0530

    functions related to array in js

---
+++ basic of javascript/array in JS/array.js
@@ -0,0 +1,27 @@
+//functions related to array in js//
+//1.find length of an array//
+let arr=[1,2,3,4,5];
+let size= arr.length;
+console.log(size);
+
+//2. to insert elements at last of array :- use push()function//
+let x=[2,4,6,8,10];
+x.push(7);
+console.log(x);
+
+//3. to insert elements from front of array :- use unshift()function//
+let y=[12,24,46,18,10];
+y.unshift(7);
+console.log(y);
+
+
+//4. to remove elements from front of array :- use shift()function//
+let z=[21,41,61,81,10];
+z.shift(7);
+console.log(z);
+
+
+//5. to remove elements from last of array :- use pop()function//
+let a=[22,43,65,48,10];
+a.pop(7);
+console.log(a);
