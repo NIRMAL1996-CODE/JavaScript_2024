5f7058363a1e3c8651bce9d088ed00d13ee3d4cd
nirmal <nirmaljaiswal888@gmail.com>
2024-07-06 01:21:48 +0530

    html file for operators

---
+++ basic of javascript/arithmetical operators/operators.js
@@ -0,0 +1,8 @@
+let a= 3;
+let b =12;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(b/a);
+console.log(a%b);
+console.log(a**b);// ** means a's power is b//
---
+++ basic of javascript/datatypes/datatypes.js
@@ -0,0 +1,15 @@
+//write a program to find the datatype of variables in js.//
+//typeof() function used to find the datatypes of varibles//
+let a=1;
+b="nirmal";
+c= 8.9;
+d= [1,2,3,4,5];
+e= true;
+f= 'q';//in js, txt in any quotes is string.//
+
+ console.log(a,typeof(a));
+ console.log(a,typeof(b));
+ console.log(a,typeof(c));
+ console.log(a,typeof(d));
+ console.log(a,typeof(e));
+ console.log(a,typeof(f));
