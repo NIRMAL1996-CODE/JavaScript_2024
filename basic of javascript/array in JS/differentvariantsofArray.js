79bd406cb8eeee733dab2b091da2cbf7efbc7729
nirmal <nirmaljaiswal888@gmail.com>
2024-07-06 03:36:24 +0530

    Different variants of array in JS

---
+++ basic of javascript/array in JS/differentvariantsofArray.js
@@ -0,0 +1,16 @@
+//to modify constant array is possible in JS ,,only possible with array not with other variables//
+const aarr=[2,3,4,5,6];
+aarr[0]=9;
+console.log(aarr);
+
+//but can't change is datatype//
+/*arr= 2.54;
+console.log(arr);  error will occure*/
+
+//in JS, you can put any type of values of different datatype in same array//
+crr=[1,"nirmal", 8.8, true,[2,5,77,88,99]];
+console.log(crr);
+
+//two dimensional array:- In JS, its not compulsory to have same length of such two dimensions//
+brr=[[1,2,2],[9,8,7,5,2]];
+console.log(brr);
