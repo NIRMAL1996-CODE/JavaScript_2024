c05888631ea783044a8761b7d311a98a8d9b3264
nirmal <nirmaljaiswal888@gmail.com>
2024-07-06 02:20:50 +0530

    write a program using for, while, do..while loops in JS

---
+++ basic of javascript/loops in Js/loop.js
@@ -0,0 +1,31 @@
+//write a different types of loops in JS//
+
+//1. for loop//
+//write a program to print 10 natural numbers//
+let num;
+console.log("using for loop");
+console.log("TEN NATURAL NUMBERS = ");
+for(num=1;num<=10;num++)
+{
+    console.log(num);
+}
+
+//2.while loop//
+let n=1;
+console.log("using while loop");
+console.log("TEN NATURAL NUMBERS = ");
+while(n<=10)
+{
+    console.log(n);
+    n++;
+}
+
+//3. do...while loop//
+
+let x=1;
+console.log("using do while loop");
+console.log("TEN NATURAL NUMBERS = ");
+do{
+    console.log(x);
+    x++;
+}while(x<=10);
