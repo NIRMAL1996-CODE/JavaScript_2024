ac1bb02cd5ad9a7cfec818fe0f7e190eb46dd68c
nirmal <nirmaljaiswal888@gmail.com>
2024-07-07 12:49:03 +0530

    write a program to find number is odd or even using ternary operators in javascript

---
+++ basic of javascript/arithmetical operators/ternaryOperator.js
@@ -0,0 +1,4 @@
+//write a program to find number is odd or even using ternary operators in javascript//
+let num1=9;
+let result= (num1%2==0)? "EVEN" : "ODD";
+console.log("THE NUMBER "+ num1 + " IS " + result);
