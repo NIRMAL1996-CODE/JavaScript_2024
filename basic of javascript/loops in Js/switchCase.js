add5cb8d01281c1d06b070d8f7ed6d45bd6cd6e8
nirmal <nirmaljaiswal888@gmail.com>
2024-07-07 12:39:22 +0530

    write a swtich case in javascript.

---
+++ basic of javascript/loops in Js/switchCase.js
@@ -0,0 +1,30 @@
+//write a program using swtich case//
+let month= 10;
+switch(month)
+{
+    case 1:  console.log("january");
+    break;
+    case 2:  console.log("febuary");
+    break;
+    case 3: console.log("march");
+    break;
+    case 4: console.log("april");
+    break;
+    case 5:  console.log("may");
+    break;
+    case 6: console.log("june");
+    break;
+    case 7: console.log("july");
+    break;
+    case 8: console.log("august");
+    break;
+    case 9: console.log("september");
+    break;
+    case 10: console.log("october");
+    break;
+    case 11: console.log("november");
+    break;
+    case 12: console.log("december");
+    default: console.log("INVALID MONTH");
+}
+
