95b5bd3598be74152bcbcd9da73a18751d4a0fc0
nirmal <nirmaljaiswal888@gmail.com>
2024-07-09 01:02:16 +0530

    template literals in js

---
+++ basic of javascript/template literals/temp.js
@@ -0,0 +1,7 @@
+//template literals in js are enclosed by backticks(``) and allow embedded expressions using ${expression}//
+let age = 10;
+let exp = 10;
+// 1st method toprint a statment using console.log//
+//console.log("my age is " +age+ " and my experience is " +exp+ "years");
+// 2nd method using backticks//
+console.log(`my age is ${age} and my experience is ${exp}`);
