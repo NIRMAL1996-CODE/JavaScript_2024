02e1532d3b48a1aa73915f2e960d41bc7634def8
nirmal <nirmaljaiswal888@gmail.com>
2024-07-10 19:51:50 +0530

    find key property is present in the object or not

---
+++ basic of javascript/objects in js/conceptsOfKEY.js
@@ -0,0 +1,18 @@
+//find key property is present in the object or not//
+//to find key on object use 'in' operator//
+const ludo= {
+     colour1:"red",
+    colour2:"green",
+    colour3:"blue",
+    colour4:"yellow"
+};
+//The syntax is:  "key" in object//
+alert(ludo.colour1);
+let key= colour1;
+
+
+
+let u = { name: "John", age: 30 };
+
+alert( "age" in u ); 
+alert( "blabla" in u ); 
