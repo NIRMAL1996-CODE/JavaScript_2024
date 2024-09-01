326a90433a860152fae8b07a009612c0681097a1
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2024-09-01 22:44:38 +0530

    Includes function of a string using JS

---
+++ basic of javascript/Explore includes functions of a string/script.js
@@ -0,0 +1,5 @@
+// inludes function is gives u true and false //
+const sentence =" hey!, its me.";
+const word = "me";
+
+console.log(`The Word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
