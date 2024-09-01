6412865680d3f9bf0d8bd5862c2e4037c6f670ff
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2024-09-01 21:39:30 +0530

    Enters the correct number by user using JS

---
+++ basic of javascript/correct number/script.js
@@ -0,0 +1,9 @@
+// enters the correct number by user//
+let num =29;
+ let i;
+ while(i != num)
+ {
+    console.log("try again");
+    i= prompt("Enter a number ");
+}
+ console.log("you have entered correct number ");
