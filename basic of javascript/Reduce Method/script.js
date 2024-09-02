6578ecefe40308a7eac1b3d5c68f432dc8cae399
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2024-09-02 16:26:48 +0530

    reduce method in JS

--- basic of javascript/Reduce Method/script.js
+++ basic of javascript/Reduce Method/script.js
@@ -1,9 +1,9 @@
 //reduce method in JS//
-//add value1+ value2= result//
+//add value1+ value2= result//( its can add , / * or any operation could perform)
 //now result become value1 and next index become value 2 n its continuous untill all elements get add//
 
 let a =[1, 2, 3, 4, 5, 6];
 let newarray = a.reduce((value1, value2)=>{
-  return value1 + value2;
+  return value1 + value2;// u can multiply also 
 });
 console.log(newarray);//output= 21
