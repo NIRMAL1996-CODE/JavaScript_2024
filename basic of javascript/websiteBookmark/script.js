6be1c71b212f9d073ece590a6eed761a5c41da46
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2024-09-05 00:04:30 +0530

    create website bookmarks using js

---
+++ basic of javascript/websiteBookmark/script.js
@@ -0,0 +1,28 @@
+let google = document.getElementById("google").addEventListener("click",()=>{
+    window.location.href= "https://www.google.com";
+});
+
+let fb = document.getElementById("facebook").addEventListener("click",()=>{
+    window.location.href= "https://www.facebook.com";
+});
+
+let twitter = document.getElementById("twitter").addEventListener("click",()=>{
+    window.location.href= "https://www.X.com";
+});
+
+//  let google = document.getElementById("#google");
+// let fb = document.getElementById("#facebook");
+// let twitter = document.getElementById("#twitter");
+
+// google= addEventListener("click",()=>{
+//     window.location= "https://www.google.com";
+// });
+
+// fb= addEventListener("click",()=>{
+//     window.location= "https://www.facebook.com";
+// });
+
+// twitter= addEventListener("click",()=>{
+//     window.location= "https://www.X.com";
+// });
+
