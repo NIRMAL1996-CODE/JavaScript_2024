356393a1d9ece4a3b6cf735e486d056ebaf6943b
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2024-09-19 15:04:28 +0530

    lass inherits other class using javascript

--- basic of javascript/OOPs-inheritance of class/script.js
+++ basic of javascript/OOPs-inheritance of class/script.js
@@ -11,16 +11,16 @@ class book{
         console.log(`${this.books} is soldout`)
     }
 };
-
+//use extends keyword to interit a class//
 class author extends book{
     authors(){
-        console.log(`the  author of this ${this.books} is ${this.author}`);
+        console.log(`the  author of the book ${this.books} is ${this.author}`);
     } 
 };
 
 let book1 =new book("THE SECRET", "Rhonda Byrne");
-let book2 =new author("INDIAN POLITICS", "random");
+let book2 =new author("INDIAN POLITICS", "Ram");
 
 book1.price();
 book2.soldout();
-book2.authors("THE SECRET", "Rhonda Byrne");
+book2.authors();
